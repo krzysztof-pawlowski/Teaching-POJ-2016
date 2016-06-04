0b813b848823d74ad4de5fea70be0ba7f089b6db
Krzysztof Pawlowski <krzysztof.pawlowski@gmail.com>
2016-06-04 13:33:17 +0200

    fixes

--- EgzaminPO/src/info/krzysztofpawlowski/Main.java
+++ EgzaminPO/src/info/krzysztofpawlowski/Main.java
@@ -1,5 +1,6 @@
 package info.krzysztofpawlowski;
 
+import info.krzysztofpawlowski.model.Egzaminator;
 import info.krzysztofpawlowski.model.OcenaRozwiazania;
 import info.krzysztofpawlowski.model.ProwadzacyGrupe;
 import info.krzysztofpawlowski.model.Rozwiazanie;
@@ -15,19 +16,24 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
+        Wykladowca wykladowca = new Wykladowca();
         ProwadzacyGrupe cwiczeniowiec1 = new ProwadzacyGrupe("JJ");
 
         List<ProwadzacyGrupe> prowadzacy = new LinkedList<>();
         prowadzacy.add(cwiczeniowiec1);
 
-        Student student1 = new Student("Jan", "Kowalski", cwiczeniowiec1);
-        Student student2 = new Student("Jan", "Nowak", cwiczeniowiec1);
+        List<Egzaminator> egzaminatorzy = new LinkedList<>();
+        egzaminatorzy.add(wykladowca);
+        egzaminatorzy.addAll(prowadzacy);
+
+        Student student1 = new Student("Jan", "Kowalski", cwiczeniowiec1, egzaminatorzy);
+        Student student2 = new Student("Jan", "Nowak", cwiczeniowiec1, egzaminatorzy);
 
         List<Student> studenci = new LinkedList<>();
         studenci.add(student1);
         studenci.add(student2);
 
-        Wykladowca wykladowca = new Wykladowca();
+
 
         Zadanie zadanie = new Zadanie("tresc");
 
--- EgzaminPO/src/info/krzysztofpawlowski/model/Student.java
+++ EgzaminPO/src/info/krzysztofpawlowski/model/Student.java
@@ -1,5 +1,7 @@
 package info.krzysztofpawlowski.model;
 
+import java.util.List;
+
 /**
  * Created by Krzysztof Pawlowski on 03/06/16.
  */
@@ -9,12 +11,15 @@ public class Student {
     private String nazwisko;
     private ProwadzacyGrupe prowadzacyGrupe;
 
+    private List<Egzaminator> egzaminatorzy;
+
     private Zadanie zadanie;
 
-    public Student(String imie, String nazwisko, ProwadzacyGrupe prowadzacyGrupe) {
+    public Student(String imie, String nazwisko, ProwadzacyGrupe prowadzacyGrupe, List<Egzaminator> egzaminatorzy) {
         this.imie = imie;
         this.nazwisko = nazwisko;
         this.prowadzacyGrupe = prowadzacyGrupe;
+        this.egzaminatorzy = egzaminatorzy;
     }
 
     public Rozwiazanie rozwiazZadanie(Zadanie zadanie) {
